196a0935105fcd833effca84887dce44ff11d009
suprobhat <61926568+suprobhat@users.noreply.github.com>
2022-01-29 22:14:05 +0530

    update code

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const http = require('http')
 const PORT = process.env.PORT || 9000
 http.createServer((req, res) => {
   console.log('New connection')
-  res.end('Hello Cloud Run v2 ' + new Date() )
+  res.end('Hello Cloud Run v2 my name suprobhat' + new Date() )
 }).listen(PORT, () => console.log('Listening on', PORT))
